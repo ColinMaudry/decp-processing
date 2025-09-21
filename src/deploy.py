5664f97ea1f7dda9693dfe068ddfd9251cd10c56
Colin Maudry <colin@maudry.com>
2025-09-21 11:08:59 +0200

    Tentative de deploiement via GitRepository(), mais ça fail avec entrypoint
    TypeError: argument should be a str or an os.PathLike object where __fspath__ returns a str, not 'NoneType'

--- src/deploy.py
+++ src/deploy.py
@@ -1,10 +1,13 @@
 from dotenv import dotenv_values
 from prefect import flow
+from prefect.runner.storage import GitRepository
 
 if __name__ == "__main__":
     env = dotenv_values()
     flow.from_source(
-        source="https://github.com/ColinMaudry/decp-processing.git",
+        source=GitRepository(
+            url="https://github.com/ColinMaudry/decp-processing.git", branch="main"
+        ),
         entrypoint="src/flows.py:decp_processing",
     ).deploy(
         name="decp-processing",
@@ -15,12 +18,37 @@ if __name__ == "__main__":
     )
 
     flow.from_source(
+        source=GitRepository(
+            url="https://github.com/ColinMaudry/decp-processing.git",
+            branch="dev",
+        ),
+        entrypoint="src/flows.py:decp_processing",
+    ).deploy(
+        name="decp-processing-dev",
+        description="Déploiement de la branche dev.",
+        work_pool_name="local",
+        ignore_warnings=True,
+    )
+
+    flow.from_source(
         source="https://github.com/ColinMaudry/decp-processing.git",
         entrypoint="src/flows.py:sirene_preprocess",
     ).deploy(
         name="sirene-preprocess",
-        description="Tous les mois, le 3",
+        description="Préparation des données SIRENE. Tous les mois, le 3",
         work_pool_name="local",
         ignore_warnings=True,
         cron="0 1 3 * *",
+    )
+
+    flow.from_source(
+        source=GitRepository(
+            url="https://github.com/ColinMaudry/decp-processing/tree/dev", branch="dev"
+        ),
+        entrypoint="src/flows.py:sirene_preprocess",
+    ).deploy(
+        name="sirene-preprocess-dev",
+        description="Préparation des données SIRENE.",
+        work_pool_name="local",
+        ignore_warnings=True,
     )
