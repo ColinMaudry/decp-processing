c83b7768377efb8943cfee608fe680c69c8eb417
Colin Maudry <colin@maudry.com>
2026-01-14 15:57:04 +0100

    Ignore les fichiers inférieurs à 150 => 180 octets (e-marchespublics.com)

--- src/flows/decp_processing.py
+++ src/flows/decp_processing.py
@@ -65,8 +65,8 @@ def decp_processing(enable_cache_removal: bool = True):
     batch_size = 100
     parquet_files = []
 
-    # Filtrer les ressources à traiter, en ne gardant que les fichiers > 150 octets
-    resources_to_process = [r for r in resources if r["filesize"] > 150]
+    # Filtrer les ressources à traiter, en ne gardant que les fichiers > 180 octets
+    resources_to_process = [r for r in resources if r["filesize"] > 180]
 
     for i in range(0, len(resources_to_process), batch_size):
         process_batch(
